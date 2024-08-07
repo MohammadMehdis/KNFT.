7295c2fcc8c3f979606c1311f38313f415355124
MohammadMehdis <your-email@example.com>
2024-08-07 17:17:49 +0330

    Finish nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        spaceGrotesk: ["Space Grotesk", "sans-serif"],
+      },
+      colors: {
+        white: {
+          100: "#FFFFFF",
+          200: "rgba(255, 255, 255, 0.43%)",
+        },
+        blue: "#1EE0E1",
+      },
+    },
   },
   plugins: [],
 };
